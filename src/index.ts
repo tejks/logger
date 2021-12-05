3d000a7acbad5be938aea3e61d3bc5bccc266709
Tejks <sarmatys.programmer@gmail.com>
2021-12-05 04:17:17 +0100

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,2 @@
-import { ErrorType } from './data';
-import { Logger } from './logger';
-
-const logger = new Logger({});
-
-logger.log({ message: 'Update failed', type: ErrorType.ERROR });
-// const myFormat = printf(({ level, message, label, timestamp }) => {
-//   return `${timestamp}
-//   ${level.toUpperCase()}: ${message}`;
-// });
-
-// const generateFilename = () => {
-//   const date = new Date();
-
-//   return `./src/logs/${date.getFullYear()}/${monthNames[date.getMonth()]}/${date.getUTCDate()}_${
-//     date.getUTCMonth() + 1
-//   }_${date.getFullYear()}.log`;
-// };
-
-// export const logger = winston.createLogger({
-//   format: combine(timestamp(), myFormat),
-//   transports: [new winston.transports.File({ filename: generateFilename() })],
-// });
+export { ErrorType } from './data';
+export { Logger } from './logger';
